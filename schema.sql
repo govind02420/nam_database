b2d4e22bf2f940b21de9f0f382babaf44067485e
govind <govind.02420@gmail.com>
2019-12-02 14:09:26 +0530

    databse

---
+++ schema.sql
@@ -0,0 +1,4 @@
+create table dev (id int ,name varchar(20),email varchar(35));
+insert into dev values(1,'abc1','abc1@gmail.com');
+insert into dev values(2,'abc2','abc2@gmail.com');
+insert into dev values(3,'abc3','abc3@gmail.com');
